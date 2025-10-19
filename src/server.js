df4c7c64899d59dd29206f60cf66c62212b74667
fareedgul <fareedgul735@gmail.com>
2025-10-18 17:53:50 -0700

    push

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
-import { LOCALHOST, PORT } from "./lib/constant.js";
+import { HOST, PORT } from "./lib/constant.js"; 
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, LOCALHOST, () => {
-    console.log(`server is on ${LOCALHOST} ${PORT}`);
+  app.listen(PORT, HOST, () => {
+    console.log(`🚀 Server is running on ${HOST}:${PORT}`);
   });
 };
 
